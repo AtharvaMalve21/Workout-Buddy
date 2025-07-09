04bb5a037a0e73211796507913a03f4d95e90715
AtharvaMalve21 <atharvam2103@gmail.com>
2025-07-09 12:58:09 +0530

    modify user.controler.js

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -46,7 +46,7 @@ export const signup = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: process.env.NOD_ENV === "production" ? "none" : "strict",
     });
 
     return res.status(201).json({
@@ -102,7 +102,7 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: process.env.NOD_ENV === "production" ? "none" : "strict",
     });
 
     return res.status(200).json({
@@ -120,7 +120,11 @@ export const login = async (req, res) => {
 
 export const logout = async (req, res) => {
   try {
-    res.clearCookie("token");
+    res.clearCookie("token", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NOD_ENV === "production" ? "none" : "strict",
+    });
     return res.status(200).json({
       success: true,
       message: "User is successfully logged out.",
